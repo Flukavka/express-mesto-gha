278e7dae68851968d41d105de3fb3b1b84c6d82b
Nosareva Victoria <nosareva.vs@yandex.ru>
2023-07-20 12:49:12 +0300

    Внести правки в комментарий

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.use('/', routes);
 
 //  слушаем ПОРТ для подключения
 app.listen(PORT, () => {
-  // eslint-disable-next-line consistent-return
+  // eslint-disable-next-line
   console.log(`App listening ${PORT}`);
 });
