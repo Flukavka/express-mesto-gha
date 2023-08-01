91bc1147808df048941db9d77d1a0ac712f266f8
Nosareva Victoria <nosareva.vs@yandex.ru>
2023-08-01 21:05:40 +0300

    Исправить ошибки после автотеста v03

--- routes/cards.js
+++ routes/cards.js
@@ -18,28 +18,28 @@ router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().pattern(/(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/),
-    owner: Joi.string().length(24)/* .hex() */,
+    owner: Joi.string().length(24).hex(),
   }).unknown(true),
 }), express.json(), createCard);
 
 //  удаляет карточку по идентификатору
 router.delete('/:cardId', celebrate({
   body: Joi.object().keys({
-    id: Joi.string().length(24)/* .hex() */,
+    id: Joi.string().length(24).hex(),
   }).unknown(true),
 }), deleteCard);
 
 //  поставить лайк карточке
 router.put('/:cardId/likes', celebrate({
   body: Joi.object().keys({
-    id: Joi.string().length(24)/* .hex() */,
+    id: Joi.string().length(24).hex(),
   }).unknown(true),
 }), likeCard);
 
 //  убрать лайк с карточки
 router.delete('/:cardId/likes', celebrate({
   body: Joi.object().keys({
-    id: Joi.string().length(24)/* .hex() */,
+    id: Joi.string().length(24).hex(),
   }).unknown(true),
 }), dislikeCard);
 
