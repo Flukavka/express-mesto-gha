c5d5613bdb5dde9d173967eef7544c0e953d78aa
Nosareva Victoria <nosareva.vs@yandex.ru>
2023-08-01 23:12:07 +0300

    Исправить ошибки после автотеста v07

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -11,17 +11,6 @@ module.exports.errorMiddleware = (error, _req, res, next) => {
     return next(new BadRequestError('Ошибка валидации'));
   }
 
-  /* if (error.code === 11000) {
-    //return next(new ConflictError('Пользователь с такой почтой уже зарегистрирован'));
-  } */
-
-  /*  if (error.statusCode === UNAUTHORIZED_ERROR) {
-     console.log(1)
-     return res
-       .status(UNAUTHORIZED_ERROR)
-       .send({ message: 'Неправильные почта или пароль' });
-   } */
-
   if (error instanceof mongoose.Error.CastError) {
     return next(new BadRequestError('Передан некорректный id'));
   }
